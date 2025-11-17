68be93e904a5081224d6a49a5a42d7a03ff38c9d
puppygrrl <puppygirl@tuta.io>
2025-11-16 19:58:36 -0800

    feat: added guessing logic

--- game/game.go
+++ game/game.go
@@ -2,19 +2,20 @@ package game
 
 import (
 	"github.com/puppygrrl/zen/dictionary"
+	"strings"
 )
 
 const maxGuesses int = 6
 
 type Game struct {
-	dict *dictionary.Dictionary
+	dict    *dictionary.Dictionary
 	guesses int
-	word string
+	word    string
 }
 
 func New() *Game {
 	newGame := Game{
-		dict: dictionary.New(),
+		dict:    dictionary.New(),
 		guesses: maxGuesses,
 	}
 	newGame.word = newGame.dict.GetRandomCommonWord()
@@ -23,4 +24,22 @@ func New() *Game {
 
 func (g *Game) Word() string {
 	return g.word
+}
+
+func (g *Game) Guess(word string) string {
+	if word == g.word {
+		return ""
+	}
+	hint := ""
+	for pos, char := range word {
+		if g.word[pos] == byte(char) {
+			hint += "1" // green
+		} else if strings.Contains(word, string(char)) {
+			hint += "2" // yellow
+		} else {
+			hint += "0" // grey
+		}
+	}
+
+	return hint
 }
